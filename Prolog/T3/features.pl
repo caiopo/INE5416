e4be8e42d50b3df400dd95a6a64f4d29ef85f1da
maorodriguesj <maorodriguesj@gmail.com>
2016-12-09 02:23:10 -0200

    rearranging

--- Prolog/T3/features.pl
+++ Prolog/T3/features.pl
@@ -6,13 +6,6 @@ mean(L, M) :-
     length(L, Len),
     M is Sum / Len.
 
-sd_list(L, SD) :-
-    mean(L, Mean),
-    length(L, Len),
-    findall(EF, (member(E, L), EF is ((E - Mean) ** 2)), Bag),
-    sum_list(Bag, Sum),
-    SD is sqrt(Sum / (Len - 1)).
-
 distance((PX, PY), (CX, CY), D) :-
     X is abs(PX - CX),
     Y is abs(PY - CY),
@@ -23,14 +16,21 @@ distances(Lborder, (CX, CY), L) :-
     findall(D, (member((X, Y, _), Lborder),
                 distance((X, Y), (CX, CY), D)), L).
 
-averageDistanceImg(Filename, AvgD) :-
-    readPGM(Filename, M), coord(M, S), averageDistance(S, AvgD).
+sd_list(L, SD) :-
+    mean(L, Mean),
+    length(L, Len),
+    findall(EF, (member(E, L), EF is ((E - Mean) ** 2)), Bag),
+    sum_list(Bag, Sum),
+    SD is sqrt(Sum / (Len - 1)).
 
 averageDistance(S, AvgD) :-
     findBorder(S, Lborder),
     centroidList(S, CX, CY),
     distances(Lborder, (CX, CY), AuxL),
     mean(AuxL, AvgD).
+
+averageDistanceImg(Filename, AvgD) :-
+    readPGM(Filename, M), coord(M, S), averageDistance(S, AvgD).
 
 sdImg(Filename, Sd) :-
     readPGM(Filename, M), coord(M, L), findBorder(L, Lborder),
